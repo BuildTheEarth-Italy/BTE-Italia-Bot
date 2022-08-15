2fb7ab5c62e759e19854ff082041d7500293dec3
forgiobombi <77931027+forgiobombi@users.noreply.github.com>
2022-08-15 21:17:46 +0200

    Added antiserverads function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import logging
 import os
+import subprocess
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s » %(message)s')
@@ -16,8 +17,9 @@ bot = commands.Bot(command_prefix='£', intents=intents)
 async def run_once_when_ready():
     await bot.wait_until_ready()
     print('BTE Italia Bot In Funzione!')
-    bot.change_presence(activity=discord.Activity(
-        type=discord.ActivityType.watching, name='bteitalia.it'))
+    
+    await bot.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.watching, name='bteitalia.tk'))
 
 
 for filename in os.listdir('./cogs'):
@@ -27,6 +29,15 @@ for filename in os.listdir('./cogs'):
 
     else:
         logging.error(f"Unable to load {filename}.")
+        
+@bot.listen('on_message')
+async def antiserverads(message):
+    if message.author.id == (833995282773049344):
+        return
+
+    elif "discord.gg" in message.content.lower():
+        await message.delete()
+        await message.channel.send(":x: **Non publicizzare il tuo server!**")        
 
 bot.loop.create_task(run_once_when_ready())
 bot.run(os.environ.get('TOKEN'))
